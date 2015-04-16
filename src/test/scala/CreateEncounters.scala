b6c36d7083ddea0b8db1c0eecce352035815f487
rnjn <runjan@gmail.com>
2015-04-16 12:04:15 +0530

    bdshr-617 | refactoring test definitions

--- src/test/scala/CreateEncounters.scala
+++ src/test/scala/CreateEncounters.scala
@@ -15,35 +15,36 @@ class CreateEncounters extends Simulation {
     .contentTypeHeader("application/xml;charset=UTF-8")
     .acceptEncodingHeader("gzip")
 
+  val login = http("login")
+    .post("http://172.18.46.56:8080/signin")
+    .header("X-Auth-Token", "1c2a599423203f639dcdd8574ac5391dd67d21316ea30ee364c8a8787fb79dd3")
+    .header("client_id", "18549")
+    .formParam("email", "dmishra@thoughtworks.com")
+    .formParam("password", "thoughtworks").check(jsonPath("$.access_token")
+    .saveAs("token"))
+
+  val register = http("registration")
+    .post("/patients/${HEALTHID}/encounters")
+    .header("X-Auth-Token", "${token}")
+    .body(ELFileBody("request-bodies/reg.xml"))
+
+  val smallEncounter = http("encounter")
+    .post("/patients/${HEALTHID}/encounters")
+    .header("X-Auth-Token", "${token}")
+    .body(ELFileBody("request-bodies/enc.xml"))
+
+  val bigEncounter = http("big encounter")
+    .post("/patients/${HEALTHID}/encounters")
+    .header("X-Auth-Token", "${token}")
+    .body(ELFileBody("request-bodies/bigenc.xml"))
+
 
   val time = 240 seconds
 
   val createEncounters = scenario("create encounters")
     .feed(patientId)
     .during(time) {
-    
-      exec(http("login")
-        .post("http://172.18.46.56:8080/signin")
-        .header("X-Auth-Token", "1c2a599423203f639dcdd8574ac5391dd67d21316ea30ee364c8a8787fb79dd3")
-        .header("client_id", "18549")
-        .formParam("email", "dmishra@thoughtworks.com")
-        .formParam("password", "thoughtworks").check(jsonPath("$.access_token")
-        .saveAs("token")))
-        
-      .exec(http("registration")
-        .post("/patients/${HEALTHID}/encounters")
-        .header("X-Auth-Token", "${token}")
-        .body(ELFileBody("request-bodies/reg.xml")))
-        
-      .exec(http("encounter")
-        .post("/patients/${HEALTHID}/encounters")
-        .header("X-Auth-Token", "${token}")
-        .body(ELFileBody("request-bodies/enc.xml")))
-        
-      .exec(http("big encounter")
-      .post("/patients/${HEALTHID}/encounters")
-      .header("X-Auth-Token", "${token}")
-      .body(ELFileBody("request-bodies/bigenc.xml")))
+    exec(login).exec(register).exec(smallEncounter).exec(bigEncounter)
   }
   setUp(
     createEncounters.inject(atOnceUsers(100)).protocols(httpConf)
